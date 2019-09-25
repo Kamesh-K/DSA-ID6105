4a6c05e93a870d8c1ca48b32af23b5aed8918dfa
Kamesh K <mm16b107@smail.iitm.ac.in>
2019-09-25 23:31:23 +0530

    Add files via upload

--- Assignment_2/Question_2.c
+++ Assignment_2/Question_2.c
@@ -3,11 +3,11 @@
 #include <time.h>
 #define loop(i,a,b) for(i=a;i<b;i++)
 #define N_MAX 10
-void input(int *n_years, int *num_iter)
+void input(int *n_years, int *num_iter, int *n_avg)
 {
   FILE *input_file;
   input_file=fopen("Input.txt","r");
-  fscanf(input_file,"%d %d",n_years,num_iter);
+  fscanf(input_file,"%d %d %d",n_years,num_iter,n_avg);
 }
 void rand_generator(int arr[],int n)
 {
@@ -22,11 +22,35 @@ void rand_generator(int arr[],int n)
 //    fprintf(output_file,"%d,%d\n",i+1,arr[i]);
     }
 }
-void SimStock(int *pStockVal,int set_s[], int n_days, int n_iter)
+void MovingAverage(int *pStockVal, double *mv_avg, int n_days, int n_iter, int n_avg)
+{
+  int i=0,j;
+  double avg;
+  loop(i,0,n_days)
+    {
+      avg=0.00;
+      loop(j,0,n_avg)
+        {
+          avg+=pStockVal[i];
+        }
+        mv_avg[i/n_avg]=(double) (avg/n_avg);
+        i=i+n_avg-1;
+    }
+    FILE *output_file;
+    char filename[]="MovingAverageX.txt";
+    filename[13]='0'+n_iter;
+    output_file=fopen(filename,"w");
+    fprintf(output_file, "Days,Stock Price\n");
+    loop(i,0,(n_days/n_avg))
+      fprintf(output_file,"%d,%lf\n",i+1,mv_avg[i]);
+}
+void SimStock(int *pStockVal,int set_s[], int n_days, int n_iter, int n_avg)
 {
   int i;
   int *rand_arr;
+  double *mv_avg;
   rand_arr=(int*) malloc(n_days*sizeof(int));
+  mv_avg=(double*)malloc((n_days/n_avg)*sizeof(double));
   rand_generator(rand_arr,n_days);
   pStockVal[0]=set_s[rand_arr[0]];
   loop(i,1,n_days)
@@ -40,18 +64,19 @@ void SimStock(int *pStockVal,int set_s[], int n_days, int n_iter)
   fprintf(output_file, "Days,Stock Price\n");
   loop(i,0,n_days)
     fprintf(output_file,"%d,%d\n",i+1,pStockVal[i]);
+  MovingAverage(pStockVal,mv_avg,n_days,n_iter,n_avg);
 }
 int main()
 {
-  int n_years,num_iter,n_days,n_iter;
+  int n_years,num_iter,n_days,n_iter,n_avg;
   int *pStockVal;
   int set_s[]={-2,-1,0,1,2};
   n_years=10;
   num_iter=1;
-  input(&n_years,&num_iter);
+  input(&n_years,&num_iter,&n_avg);
   n_days=n_years*365;
   pStockVal=(int*)malloc(n_days*sizeof(int));
   loop(n_iter,0,num_iter)
-    SimStock(pStockVal,set_s,n_days,n_iter);
+    SimStock(pStockVal,set_s,n_days,n_iter,n_avg);
   return 0;
 }
--- Assignment_2/SimStockPlot.m
+++ Assignment_2/SimStockPlot.m
@@ -1,5 +1,8 @@
 function [y] = SimStockPlot(n)
+    clc;
+    close all;
     i=0;
+    n=n-1;
     s='F:\Academics\SEM 7\D_ID6105\Assignment_2\SimStockVal';
     txt='.txt';
     str='legend';
@@ -23,4 +26,27 @@ function [y] = SimStockPlot(n)
     xlabel('Days');
     ylabel('Stock Price');
     title('Simulation of Stocks');
+    figure,
+    i=0;
+    s='F:\Academics\SEM 7\D_ID6105\Assignment_2\MovingAverage';
+    legend_mat=[];
+    for i=0:n
+    filename=strcat(s,num2str(i),txt);    
+    delimiter = ',';
+    startRow = 2;
+    formatSpec = '%f%f%[^\n\r]';
+    fileID = fopen(filename,'r');
+    dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+    fclose(fileID);
+    Days = dataArray{:, 1};
+    StockPrice = dataArray{:, 2};
+    hold on 
+    plot(Days,StockPrice,'-*');
+    str=strcat('Stock ',num2str(i));
+    legend_mat=[legend_mat; str];
+    end
+    legend(legend_mat,'Location','SouthWest');
+    xlabel('Time Period');
+    ylabel('Stock Price');
+    title('Moving Average of Stocks');
 end
